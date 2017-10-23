d4972d93b997924ce9992ab8ee4cd1cd15d8da8f
Gawin <gawin@4demgawin.ad.here.com>
2017-10-23 17:29:53 +0200

    Second commit

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,6 +1,11 @@
 import random as rd
 
-nums = []
-for t in range(10):
-    nums.append(rd.randint(0, 100))
-    print(sorted(nums))
+def test():
+    nums = [] for t in range(10):
+        nums.append(rd.randint(0, 100))
+        print(sorted(nums))
+
+test()
+
+
+print('hello'*8)
