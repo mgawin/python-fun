cb9760ac4535dbbc101e240367e48d00f96c99c5
Gawin <gawin@4demgawin.ad.here.com>
2017-10-23 17:43:37 +0200

    fsdfsdfs

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,11 +1,13 @@
 import random as rd
 
+
 def test():
     nums = [] for t in range(10):
         nums.append(rd.randint(0, 100))
         print(sorted(nums))
 
+
 test()
 
 
-print('hello'*8)
+print('hello' * 8)
